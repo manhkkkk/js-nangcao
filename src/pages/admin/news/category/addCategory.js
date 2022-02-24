bea2de9c6dfcc9390d95e9515bfba24ebf629c18
manhkkkk <pktopad@gmail.com>
2022-02-24 20:21:14 +0700

    Final

---
+++ src/pages/admin/news/category/addCategory.js
@@ -0,0 +1,87 @@
+
+import NavAdmin from "../../../../components/NavAdmin";
+import axios from "axios";
+import { add } from "../../../../api/categories";
+
+const AdminCategoryAdd = {
+	render() {
+		return /*html*/ `
+        <div class="min-h-full">
+            ${NavAdmin.render()}
+            <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="lg:flex lg:items-center lg:justify-between">
+                    <div class="flex-1 min-w-0">
+                        <h2
+                        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
+                        >
+                        Thêm mới
+                        </h2>
+                    </div>
+                    <div class="mt-5 flex lg:mt-0 lg:ml-4">
+                        <a href="/admin/news/add" class="sm:ml-3">
+                            <button
+                                type="button"
+                                class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Quay lại
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            </header>
+            <main>
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <!-- Replace with your content -->
+                <div class="px-4 py-6 sm:px-0">
+                    <form id="form-add-product">
+                        <input type="text" class="shadow-sm p-2  focus:ring-indigo-500 focus:border-indigo-500 mt-1 block sm:text-sm border border-gray-300 rounded-md" id="product-name" placeholder="Name"/><br /> <!--titlepost-->
+				<input type="number" class="border border-blackshadow-sm p-2  focus:ring-indigo-500 focus:border-indigo-500 mt-1 block sm:text-sm border border-gray-300 rounded-md" id="product-price" placeholder="Price"/><br /> <!--titlepost-->
+                        <textarea name="" cols="30" rows="10" class="shadow-sm p-8 focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" id="product-desc" placeholder="descript"></textarea><br /><!--img-desscript-->
+                        <button class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Thêm</button>
+                    </form>
+                </div>
+                <!-- /End replace -->
+            </div>
+            </main>
+        </div>
+        `;
+	},
+	afterRender() {
+		const formAdd = document.querySelector("#form-add-product");
+		const imgProduct = document.querySelector("#img-product")
+		const imgPreview = document.querySelector("#img-preview")
+		const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
+		const CLOUDINARY_PRESET = "jkbdphzy";
+
+		formAdd.addEventListener("submit", async function (e) {
+			e.preventDefault();
+			const file = imgProduct.files[0];
+			const formData = new FormData();
+			formData.append("file", file);
+			formData.append("upload_preset", CLOUDINARY_PRESET);
+
+			// call api cloudinary
+			const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
+
+				headers: {
+					"Content-Type": "application/form-data",
+
+				}
+			})
+			add({
+
+				name: document.querySelector("#product-name").value,
+				img: data.url,
+				desc: document.querySelector("#product-desc").value,
+				price: document.querySelector("#product-price").value,
+			});
+		});
+	},
+};
+export default AdminCategoryAdd;
+
+
+
